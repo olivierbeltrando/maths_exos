670317f07e41294a88427d0bfad3d51f0ddae2b6
Olivier BELTRANDO <olivierbeltrando@gmail.com>
2020-11-19 20:34:52 +0100

    v2

--- tables.py
+++ tables.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+from datetime import datetime
 
 to_do = set()
 dones = set()
@@ -23,20 +26,27 @@ class bcolors:
     UNDERLINE = "\033[4m"
 
 
+temps = []
+
+
 def exo(i, j):
     print(f"{i} x {j} =")
+    start_t = datetime.now()
     while True:
+        res = input()
         try:
-            res = input()
+            res = int(res.strip())
         except ValueError as e:
             print("ce n'est pas un nombre...")
+            continue
 
-        res = int(res.strip())
         expected = i * j
         success = res == expected
         print(f"{i} x {j} = {expected}")
         if success:
             print(f"{bcolors.HEADER}bravo{bcolors.ENDC}\n")
+            end_t = datetime.now()
+            temps.append(end_t - start_t)
             return "success"
         print(f"{bcolors.FAIL}dommage{bcolors.ENDC}\n")
         return "failure"
@@ -46,18 +56,26 @@ def exo(i, j):
         #     return
 
 
-while to_do:
-    for ele in to_do:
-        res = exo(*ele)
-        if res == "success":
-            dones.add(ele)
-        else:
-            # errors are added twice
-            failed.add(ele)
-            i, j = ele
-            failed.add((j, i))
-    to_do = failed
-    failed = set()
+while to_do or failed:
+    if to_do:
+        ele = to_do.pop()
+    elif failed:
+        ele = failed.pop()
+    os.system("clear")
+    print(f"Multiplications a faire: {bcolors.HEADER}{len(to_do)}{bcolors.ENDC}")
+    print(f"Multiplications a re-faire: {bcolors.FAIL}{len(failed)}{bcolors.ENDC}")
+    res = exo(*ele)
+    if res == "success":
+        dones.add(ele)
+    else:
+        # errors are added twice
+        failed.add(ele)
+        i, j = ele
+        failed.add((j, i))
 
 print(f"{bcolors.HEADER}BRAVO c'est terminé {bcolors.ENDC}")
 print(f"Tu as fait {bcolors.HEADER}{len(dones)}{bcolors.ENDC} multiplications")
+seconds = [t.total_seconds() for t in temps]
+somme = int(sum(seconds))
+average = int(sum(seconds) / len(seconds))
+print(f"Tu as mis {somme}s soit {average}s par réponse")
